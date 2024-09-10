4457b596d8807891c8b5490725240b86bc879534
SACHIN <ghoshsachin2301@gmail.com>
2024-09-10 23:34:57 +0530

    removed tawk

--- projects/script.js
+++ projects/script.js
@@ -116,15 +116,15 @@ getProjects().then(data => {
 //     s0.parentNode.insertBefore(s1, s0);
 // })();
 
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/66df4bd5ea492f34bc1007d4/1i7c40fao';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/66df4bd5ea492f34bc1007d4/1i7c40fao';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
 // End of Tawk.to Live Chat
 
 // disable developer mode
